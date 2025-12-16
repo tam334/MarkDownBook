a5f6c477597fd69f9a050c6def28d60e99eea861
y_tsukuda <y.tsukuda.gm.01@gmail.com>
2025-12-16 21:16:29 +0900

    固定パス、固定値での外部プログラム呼び出し ボタンの配置に必要なプロパティの調査

--- Src/MarkDownBookLauncher/MarkDownBookLauncher/MainWindow.xaml.cs
+++ Src/MarkDownBookLauncher/MarkDownBookLauncher/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Numerics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +21,26 @@ namespace MarkDownBookLauncher
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OnClickFirefox(object sender, RoutedEventArgs e)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
+            string path = "C:\\Users\\ytsuk\\Documents\\MyWork\\MarkdownBook\\Doc\\Plan\\";
+            process.StartInfo.Arguments = path + "00_概要.md";
+            process.StartInfo.Arguments += " ";
+            process.StartInfo.Arguments += path + "01_日報.md";
+            process.Start();
+        }
+
+        private void OnClickVisualStudioCode(object sender, RoutedEventArgs e)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "C:\\Users\\ytsuk\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe";
+            string path = "C:\\Users\\ytsuk\\Documents\\MyWork\\MarkdownBook\\Doc\\Plan\\";
+            process.StartInfo.Arguments = path;
+            process.Start();
         }
     }
 }
