8b262c00acff8b2fb0889d8f870e13053615502e
y_tsukuda <y.tsukuda.gm.01@gmail.com>
2025-12-16 21:33:17 +0900

    スクリプトの配置の修正 スクリプトのロード、読み込み対象の整理

--- Src/MarkDownBookLauncher/MarkDownBookLauncher/MainWindow.xaml.cs
+++ Src/MarkDownBookLauncher/MarkDownBookLauncher/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.IO;
 using System.Numerics;
 using System.Text;
 using System.Windows;
@@ -18,19 +19,30 @@ namespace MarkDownBookLauncher
     /// </summary>
     public partial class MainWindow : Window
     {
+        string dir = "C:\\Users\\ytsuk\\Documents\\MyWork\\MarkdownBook\\Doc\\Plan\\";
+        string files = "";
+
         public MainWindow()
         {
             InitializeComponent();
+            LoadSettings();
+            files += dir + "00_概要.md";
+            files += " ";
+            files += dir + "01_日報.md";
+        }
+
+        private void LoadSettings()
+        {
+            string relPath = "Script\\Win\\";
+            string iniFirefox = File.ReadAllText(relPath + "Firefox.ini");
+            string iniVsCode = File.ReadAllText(relPath + "VisualStudioCode.ini");
         }
 
         private void OnClickFirefox(object sender, RoutedEventArgs e)
         {
             Process process = new Process();
             process.StartInfo.FileName = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
-            string path = "C:\\Users\\ytsuk\\Documents\\MyWork\\MarkdownBook\\Doc\\Plan\\";
-            process.StartInfo.Arguments = path + "00_概要.md";
-            process.StartInfo.Arguments += " ";
-            process.StartInfo.Arguments += path + "01_日報.md";
+            process.StartInfo.Arguments = files;
             process.Start();
         }
 
@@ -38,8 +50,7 @@ namespace MarkDownBookLauncher
         {
             Process process = new Process();
             process.StartInfo.FileName = "C:\\Users\\ytsuk\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe";
-            string path = "C:\\Users\\ytsuk\\Documents\\MyWork\\MarkdownBook\\Doc\\Plan\\";
-            process.StartInfo.Arguments = path;
+            process.StartInfo.Arguments = dir;
             process.Start();
         }
     }
