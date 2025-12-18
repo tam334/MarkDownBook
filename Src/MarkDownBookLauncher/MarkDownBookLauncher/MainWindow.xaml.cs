c5bf7874c9b767aedb8fa3cbbf1e2f139458f5b1
y_tsukuda <y.tsukuda.gm.01@gmail.com>
2025-12-18 21:51:06 +0900

    実行情報をiniファイルから読む

--- Src/MarkDownBookLauncher/MarkDownBookLauncher/MainWindow.xaml.cs
+++ Src/MarkDownBookLauncher/MarkDownBookLauncher/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Numerics;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +14,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
+
 namespace MarkDownBookLauncher
 {
     /// <summary>
@@ -21,6 +25,16 @@ namespace MarkDownBookLauncher
     {
         string dir = "C:\\Users\\ytsuk\\Documents\\MyWork\\MarkdownBook\\Doc\\Plan\\";
         string files = "";
+
+        class ExeInfo
+        {
+            public string path = "";
+            public string tooltip = "";
+            public string arguments = "";
+        };
+
+        ExeInfo exeFirefox = new();
+        ExeInfo exeVsCode = new();
 
         public MainWindow()
         {
@@ -36,22 +50,66 @@ namespace MarkDownBookLauncher
             string relPath = "Script\\Win\\";
             string iniFirefox = File.ReadAllText(relPath + "Firefox.ini");
             string iniVsCode = File.ReadAllText(relPath + "VisualStudioCode.ini");
+
+            string sectionName = "Settings";
+            {
+                string path = relPath + "Firefox.ini";
+                exeFirefox = new ExeInfo()
+                {
+                    path = GetIniValue(path, sectionName, "Exe"),
+                    tooltip = GetIniValue(path, sectionName, "Tooltip"),
+                    arguments = GetIniValue(path, sectionName, "Arguments")
+                };
+            }
+
+            {
+                string path = relPath + "VisualStudioCode.ini";
+                exeVsCode = new ExeInfo()
+                {
+                    path = GetIniValue(path, sectionName, "Exe"),
+                    tooltip = GetIniValue(path, sectionName, "Tooltip"),
+                    arguments = GetIniValue(path, sectionName, "Arguments")
+                };
+            }
         }
 
         private void OnClickFirefox(object sender, RoutedEventArgs e)
         {
             Process process = new Process();
-            process.StartInfo.FileName = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
-            process.StartInfo.Arguments = files;
+            process.StartInfo.FileName = exeFirefox.path;
+            process.StartInfo.Arguments = ParseArgument(exeFirefox.arguments);
             process.Start();
         }
 
         private void OnClickVisualStudioCode(object sender, RoutedEventArgs e)
         {
             Process process = new Process();
-            process.StartInfo.FileName = "C:\\Users\\ytsuk\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe";
-            process.StartInfo.Arguments = dir;
+            process.StartInfo.FileName = exeVsCode.path;
+            process.StartInfo.Arguments = ParseArgument(exeVsCode.arguments);
             process.Start();
+        }
+
+        private string ParseArgument(string argument)
+        {
+            string tmp = argument.Replace("(dir)", dir);
+            tmp = tmp.Replace("(files)", files);
+            return tmp;
+        }
+
+        [DllImport("kernel32.dll")]
+        private static extern int GetPrivateProfileString(
+            string lpApplicationName,
+            string lpKeyName,
+            string lpDefault,
+            StringBuilder lpReturnedstring,
+            int nSize,
+            string lpFileName);
+
+        public static string GetIniValue(string path, string section, string key)
+        {
+            StringBuilder sb = new StringBuilder(256);
+            GetPrivateProfileString(section, key, string.Empty, sb, sb.Capacity, path);
+            return sb.ToString();
         }
     }
 }
