c67ce86d069d85208ef22e42b7496107a63d366e
y_tsukuda <y.tsukuda.gm.01@gmail.com>
2025-12-27 21:41:31 +0900

    テキストボックスの値をバインディング 実際の作成処理を作成

--- Src/MarkDownBookLauncher/MarkdownBookCreate/MainWindow.xaml.cs
+++ Src/MarkDownBookLauncher/MarkdownBookCreate/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,24 +16,60 @@ namespace MarkdownBookCreate
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         string defaultPath = "_mkbook/";
+        string _createPath = "";
+        public string CreatePath {
+            get => _createPath;
+            set
+            {
+                _createPath = value;
+                OnPropertyChanged(nameof(CreatePath));
+            }
+        }
 
-        public MainWindow(string dirPath)
+        string _projName = "Project";
+        public string ProjName
+        {
+            get => _projName;
+            set
+            {
+                _projName = value;
+                OnPropertyChanged(nameof(ProjName));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public MainWindow()
         {
             InitializeComponent();
-            tbPath.Text = DefaultPath();
+            tbPath.DataContext = this;
+            CreatePath = DefaultPath();
+
+            tbProjName.DataContext = this;
         }
 
         private void OnClickRevert(object sender, RoutedEventArgs e)
         {
-            this.tbPath.Text = DefaultPath();
+            CreatePath = DefaultPath();
+        }
+
+        private void OnClickCreate(object sender, RoutedEventArgs e)
+        {
+            string path = System.Environment.CurrentDirectory + "\\" + CreatePath;
+            Directory.CreateDirectory(path);
+            Environment.Exit(0);
         }
 
         private string DefaultPath()
         {
-            return defaultPath + tbProjName.Text + "/";
+            return defaultPath + ProjName + "/";
         }
     }
 }
